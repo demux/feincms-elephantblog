2e48e4f14ec68664f6f37e868661a82d9f467b9a
Arnar Yngvason <arnar@hvitahusid.is>
2013-03-07 15:00:53 +0000

    Added entry.first_markupmirror so first Markup Mirror content can be rendered in Entry Archive

--- elephantblog/utils.py
+++ elephantblog/utils.py
@@ -4,6 +4,11 @@ from elephantblog.models import Category, Entry
 def entry_list_lookup_related(entry_qs):
     entry_dict = dict((e.pk, e) for e in entry_qs)
 
+    if hasattr(Entry, 'markupmirrorcontent_set'):
+        for content in Entry.markupmirrorcontent_set.related.model.objects.filter(
+                parent__in=entry_dict.keys()).reverse():
+            entry_dict[content.parent_id].first_markupmirror = content
+
     if hasattr(Entry, 'richtextcontent_set'):
         for content in Entry.richtextcontent_set.related.model.objects.filter(
                 parent__in=entry_dict.keys()).reverse():
